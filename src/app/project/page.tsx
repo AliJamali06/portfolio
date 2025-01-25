4d51f0f65a01076e30f87a9e705178f9daf2548d
Ali Dost Jamali <163766301+AliJamali06@users.noreply.github.com>
2025-01-25 16:13:43 +0500

    skills and project are now responsive

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,124 +1,120 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+
 function Project() {
   return (
     <div>
       <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+        <div className="container px-4 sm:px-6 lg:px-8 py-12 sm:py-16 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="text-2xl sm:text-3xl font-medium title-font mb-4 text-gray-900">
               My Projects
             </h1>
           </div>
-          <div className="flex flex-wrap -m-5 -mt-[5rem]">
-            {/* projects */}
-            <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
-              <div className="flex relative">
-                <Image
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  width={"600"}
-                  height={"300"}
-                  src="/image2.png"
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    Currency conveter
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    <b> Currency</b>
-                  </h1>
-                  <p className="leading-relaxed ">
-                    This is my Currency conveter projects
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+            {/* Project 1 */}
+            <div className="relative cursor-pointer group">
+              <Image
+                alt="Currency Converter"
+                className="rounded-lg"
+                width={600}
+                height={300}
+                src="/image2.png"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center items-center bg-white bg-opacity-90 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg">
+                <h2 className="text-sm font-medium text-indigo-500 mb-2">
+                  Currency Converter
+                </h2>
+                <h1 className="text-lg font-medium text-gray-900 mb-3">
+                  <b>Currency</b>
+                </h1>
+                <p className="text-gray-700 mb-2 text-center">
+                  This is my Currency Converter project.
+                </p>
+                <Link href="https://github.com/alijamali06">
+                  <p className="text-blue-500 hover:underline">
+                    View Project
                   </p>
-                  <Link href={"https://github.com/alijamali06"}>
-                    <p className="leading-relaxed text-blue-500 hover:underline">
-                      view projects
-                    </p>
-                  </Link>
-                </div>
+                </Link>
               </div>
             </div>
-            <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
-              <div className="flex relative">
-                <Image
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  width={"600"}
-                  height={"100"}
-                  src="/image22.png"
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    <b>Calculator</b>
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    <b> Calculator </b>
-                  </h1>
-                  <p className="leading-relaxed ">
-                    This is my Calculator project
+            {/* Project 2 */}
+            <div className="relative cursor-pointer group">
+              <Image
+                alt="Calculator"
+                className="rounded-lg"
+                width={600}
+                height={300}
+                src="/image22.png"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center items-center bg-white bg-opacity-90 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg">
+                <h2 className="text-sm font-medium text-indigo-500 mb-2">
+                  <b>Calculator</b>
+                </h2>
+                <h1 className="text-lg font-medium text-gray-900 mb-3">
+                  <b>Calculator</b>
+                </h1>
+                <p className="text-gray-700 mb-2 text-center">
+                  This is my Calculator project.
+                </p>
+                <Link href="https://github.com/alijamali06">
+                  <p className="text-blue-500 hover:underline">
+                    View Project
                   </p>
-                  <Link href={"https://github.com/alijamali06"}>
-                    <p className="leading-relaxed text-blue-500 hover:underline">
-                      view projects
-                    </p>
-                  </Link>
-                </div>
+                </Link>
               </div>
             </div>
-            <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
-              <div className="flex relative">
-                <Image
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  width={"600"}
-                  height={"100"}
-                  src="/image100.png"
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    <b>Static Resume</b>
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    <b> Static Resume </b>
-                  </h1>
-                  <p className="leading-relaxed ">
-                    This is my Static Resume project
+            {/* Project 3 */}
+            <div className="relative cursor-pointer group">
+              <Image
+                alt="Static Resume"
+                className="rounded-lg"
+                width={600}
+                height={300}
+                src="/image100.png"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center items-center bg-white bg-opacity-90 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg">
+                <h2 className="text-sm font-medium text-indigo-500 mb-2">
+                  <b>Static Resume</b>
+                </h2>
+                <h1 className="text-lg font-medium text-gray-900 mb-3">
+                  <b>Static Resume</b>
+                </h1>
+                <p className="text-gray-700 mb-2 text-center">
+                  This is my Static Resume project.
+                </p>
+                <Link href="https://github.com/AliJamali06/static-Resume">
+                  <p className="text-blue-500 hover:underline">
+                    View Project
                   </p>
-                  <Link href={"https://github.com/AliJamali06/static-Resume"}>
-                    <p className="leading-relaxed text-blue-500 hover:underline">
-                      view projects
-                    </p>
-                  </Link>
-                </div>
+                </Link>
               </div>
             </div>
-            <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
-              <div className="flex relative">
-                <Image
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  width={"400"}
-                  height={"200"}
-                  src="/imageman.png"
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    <b>Dynamic Resume</b>
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    <b> Dynamic Resume </b>
-                  </h1>
-                  <p className="leading-relaxed ">
-                    This is my Dynamic Resume project
+            {/* Project 4 */}
+            <div className="relative cursor-pointer group">
+              <Image
+                alt="Dynamic Resume"
+                className="rounded-lg"
+                width={600}
+                height={300}
+                src="/imageman.png"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center items-center bg-white bg-opacity-90 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg">
+                <h2 className="text-sm font-medium text-indigo-500 mb-2">
+                  <b>Dynamic Resume</b>
+                </h2>
+                <h1 className="text-lg font-medium text-gray-900 mb-3">
+                  <b>Dynamic Resume</b>
+                </h1>
+                <p className="text-gray-700 mb-2 text-center">
+                  This is my Dynamic Resume project.
+                </p>
+                <Link href="https://github.com/AliJamali06/Dynamic-resume">
+                  <p className="text-blue-500 hover:underline">
+                    View Project
                   </p>
-                  <Link href={"https://github.com/AliJamali06/Dynamic-resume"}>
-                    <p className="leading-relaxed text-blue-500 hover:underline">
-                      view projects
-                    </p>
-                  </Link>
-                </div>
+                </Link>
               </div>
             </div>
           </div>
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,141 +1,59 @@
-import React from 'react'
-import { CiSquareCheck } from 'react-icons/ci'
+import React from "react";
+import { CiSquareCheck } from "react-icons/ci";
 
 const Skills = () => {
   return (
     <div>
       <section className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-col text-center w-full mb-20">
-      <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
-        Skills
-      </h2>
-      <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">
-        My skills
-      </h1>
+        <div className="container px-4 sm:px-6 lg:px-8 py-12 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+              Skills
+            </h2>
+            <h1 className="text-2xl sm:text-3xl font-medium title-font text-gray-900">
+              My Skills
+            </h1>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {/* Skill Card */}
+            {[
+              { name: "HTML", percentage: "100%" },
+              { name: "CSS", percentage: "88%" },
+              { name: "Javascript/Typescript", percentage: "95%" },
+              { name: "NEXT JS", percentage: "85%" },
+              { name: "ArcMap", percentage: "85%" },
+              { name: "Wordpress", percentage: "85%" },
+            ].map((skill, index) => (
+              <div
+                key={index}
+                className="flex flex-col p-6 bg-white rounded-lg shadow-md"
+              >
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white">
+                    <CiSquareCheck className="text-xl font-bold" />
+                  </div>
+                  <h2 className="text-gray-900 text-lg font-medium">
+                    {skill.name}
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <div className="relative h-1 w-full bg-gray-300 rounded-xl">
+                    <div
+                      className="absolute bg-blue-500 h-1 rounded-xl"
+                      style={{ width: skill.percentage }}
+                    ></div>
+                  </div>
+                  <p className="font-bold text-blue-500 text-right mt-2">
+                    {skill.percentage}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
-    <div className="flex flex-wrap -m-4 -mt-[5rem]">
-     {/* skills */}
-      <div className="p-4 md:w-1/3">
-        <div className="flex rounded-lg h-ful p-8 flex-col">
-          <div className="flex items-center mb-3">
-            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-fullblue-blue-500 text-white flex-shrink-0">
-            <CiSquareCheck className='text-xl font-bold'/>
-            </div>
-            <h2 className="text-gray-900 text-lg title-font font-medium">
-              HTML
-            </h2>
-          </div>
-          <div className="flex-grow">
-          <div className='relative h-1 w-full bg-gray-300 rounded-xl'>
-            <div className='absolute bg-blue-500 h-1 rounded-xl w-[100%]'></div>
-        </div>
-            <p className='font-bold text-blue-500 text-right'>100%</p>
-          </div>
-        </div>
-      </div>
-       {/* skills */}
-       <div className="p-4 md:w-1/3">
-        <div className="flex rounded-lg h-ful p-8 flex-col">
-          <div className="flex items-center mb-3">
-            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-fullblue-blue-500 text-white flex-shrink-0">
-            <CiSquareCheck className='text-xl font-bold'/>
-            </div>
-            <h2 className="text-gray-900 text-lg title-font font-medium">
-              CSS
-            </h2>
-          </div>
-          <div className="flex-grow">
-          <div className='relative h-1 w-full bg-gray-300 rounded-xl'>
-            <div className='absolute bg-blue-500 h-1 rounded-xl w-[88%]'></div>
-        </div>
-            <p className='font-bold text-blue-500 text-right'>88%</p>
-          </div>
-        </div>
-      </div>
-       {/* skills */}
-       <div className="p-4 md:w-1/3">
-        <div className="flex rounded-lg h-ful p-8 flex-col">
-          <div className="flex items-center mb-3">
-            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-fullblue-blue-500 text-white flex-shrink-0">
-            <CiSquareCheck className='text-xl font-bold'/>
-            </div>
-            <h2 className="text-gray-900 text-lg title-font font-medium">
-              Javascript/Typescript
-            </h2>
-          </div>
-          <div className="flex-grow">
-          <div className='relative h-1 w-full bg-gray-300 rounded-xl'>
-            <div className='absolute bg-blue-500 h-1 rounded-xl w-[90%]'></div>
-        </div>
-            <p className='font-bold text-blue-500 text-right'>95%</p>
-          </div>
-        </div>
-      </div>
-        {/* skills */}
-        <div className="p-4 md:w-1/3">
-        <div className="flex rounded-lg h-ful p-8 flex-col">
-          <div className="flex items-center mb-3">
-            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-fullblue-blue-500 text-white flex-shrink-0">
-            <CiSquareCheck className='text-xl font-bold'/>
-            </div>
-            <h2 className="text-gray-900 text-lg title-font font-medium">
-              NEXT JS
-            </h2>
-          </div>
-          <div className="flex-grow">
-          <div className='relative h-1 w-full bg-gray-300 rounded-xl'>
-            <div className='absolute bg-blue-500 h-1 rounded-xl w-[85%]'></div>
-        </div>
-            <p className='font-bold text-blue-500 text-right'>85%</p>
-          </div>
-        </div>
-      </div>
-         {/* skills */}
-         <div className="p-4 md:w-1/3">
-        <div className="flex rounded-lg h-ful p-8 flex-col">
-          <div className="flex items-center mb-3">
-            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-fullblue-blue-500 text-white flex-shrink-0">
-            <CiSquareCheck className='text-xl font-bold'/>
-            </div>
-            <h2 className="text-gray-900 text-lg title-font font-medium">
-              ArcMap
-            </h2>
-          </div>
-          <div className="flex-grow">
-          <div className='relative h-1 w-full bg-gray-300 rounded-xl'>
-            <div className='absolute bg-blue-500 h-1 rounded-xl w-[85%]'></div>
-        </div>
-            <p className='font-bold text-blue-500 text-right'>85%</p>
-          </div>
-        </div>
-      </div>
-         {/* skills */}
-         <div className="p-4 md:w-1/3">
-        <div className="flex rounded-lg h-ful p-8 flex-col">
-          <div className="flex items-center mb-3">
-            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-fullblue-blue-500 text-white flex-shrink-0">
-            <CiSquareCheck className='text-xl font-bold'/>
-            </div>
-            <h2 className="text-gray-900 text-lg title-font font-medium">
-              Wordpress
-            </h2>
-          </div>
-          <div className="flex-grow">
-          <div className='relative h-1 w-full bg-gray-300 rounded-xl'>
-            <div className='absolute bg-blue-500 h-1 rounded-xl w-[85%]'></div>
-        </div>
-            <p className='font-bold text-blue-500 text-right'>85%</p>
-          </div>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-</section>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Skills
+export default Skills;
